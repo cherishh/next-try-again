8ece7eb9e0fef92197a041b4f5169732f8d9ccdc
cherishh <wzx1991@gmail.com>
2025-05-24 22:30:43 +0800

    feat: try pathless nested layout

--- app/(pathless)/(nested)/layout.tsx
+++ app/(pathless)/(nested)/layout.tsx
@@ -1,7 +1,7 @@
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='mt-2'>
-      <h2>this is a nested layout</h2>
+      <h2>this nested layout cant not show</h2>
       <div>{children}</div>
     </div>
   );
