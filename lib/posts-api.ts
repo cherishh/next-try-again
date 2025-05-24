96a4efdd18db6d569ed864aaac3f2d901d9bb065
cherishh <wzx1991@gmail.com>
2025-05-24 11:16:16 +0800

    feat: test

--- lib/posts-api.ts
+++ lib/posts-api.ts
@@ -1,6 +1,9 @@
 export async function getPost(id: string) {
   const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
     cache: 'force-cache',
+    next: {
+      revalidate: 10,
+    },
   }).then(res => res.json());
   return post;
 }
