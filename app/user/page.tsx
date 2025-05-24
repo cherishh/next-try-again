38f50ca9345d07892e56f8aaa1f47dd0ae371b63
cherishh <wzx1991@gmail.com>
2025-05-24 08:49:20 +0800

    feat: query db

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,7 +1,11 @@
-function User() {
+import { getUserById } from '@/lib/db/queries/select';
+
+async function User() {
+  const user = await getUserById(1);
   return (
     <div className='p-8'>
       <h1>User</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
 }
