a47c3712b52f0bc993564411cb68bf7d58a651c4
cherishh <wzx1991@gmail.com>
2025-05-27 20:00:43 +0800

    feat: comment out llm

--- app/llm/page.tsx
+++ app/llm/page.tsx
@@ -10,7 +10,7 @@ export default function RouteComponent() {
   return (
     <div className='m-4'>
       <h2>LLM</h2>
-      {messages.map(message => (
+      {/* {messages.map(message => (
         <div key={message.id}>
           {message.role === 'user' ? 'User: ' : 'AI: '}
           {message.content}
@@ -20,7 +20,7 @@ export default function RouteComponent() {
       <form onSubmit={handleSubmit}>
         <input name='prompt' value={input} onChange={handleInputChange} />
         <button type='submit'>Submit</button>
-      </form>
+      </form> */}
     </div>
   );
 }
