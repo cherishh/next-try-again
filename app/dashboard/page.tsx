3506f56c5f4085c69c89df59c22d1ff900fcec58
cherishh <wzx1991@gmail.com>
2025-06-29 00:24:53 +0800

    fix: auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,22 @@
+'use client';
+
+import { useSession } from '@/lib/auth-client';
+import { redirect } from 'next/navigation';
+import { toast } from 'sonner';
+import { Button } from '@/components/ui/button';
 export default function Dashboard() {
+  const { data: session, isPending } = useSession();
+  const user = session?.user;
+
+  if (!user) {
+    toast.error('Please login to access this page');
+    return (
+      <div className='flex flex-col items-center justify-center h-screen'>
+        <Button onClick={() => redirect('/')}>home</Button>
+      </div>
+    );
+  }
+
   return (
     <div className='m-4'>
       <h2>Dashboard</h2>
